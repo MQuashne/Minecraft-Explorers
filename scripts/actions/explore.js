7f21d721bfe5082d093841e7b0f885e7c16d0c6f
MQuashne <57578781+MQuashne@users.noreply.github.com>
2026-03-19 16:55:24 -0400

    Update explore.js

--- scripts/actions/explore.js
+++ scripts/actions/explore.js
@@ -1,1 +1,10 @@
-hold
+export function exploreLandscape(gameState, renderCallback, landscapeCard) {
+
+	const actionModal = document.getElementById("action-modal");
+	actionModal.classList.remove("hidden");
+	const modalCardInfo = document.getElementById("modal-card-info");
+	modalCardInfo.innerHTML=''
+	const landscapeCardView = document.createElement("img");
+	landscapeCardView.src = `images/landscapes/${landscapeCard.visual}.jpg`;
+	landscapeCardView.classList.add("card", "modal-landscape");	modalCardInfo.appendChild(landscapeCardView);
+}
