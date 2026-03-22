bc96d7143fa288b15bd5c1ba6896a93e59919b3a
MQuashne <57578781+MQuashne@users.noreply.github.com>
2026-03-22 11:05:00 -0400

    Update explore.js

--- scripts/actions/explore.js
+++ scripts/actions/explore.js
@@ -1,42 +1,8 @@
-function buildTestInv() {
-	const testInv = [{
-			id: "item_diamond_sword_02",
-			visual: "item_diamond_sword_mob",
-			name: "Diamond Sword",
-			category: "sword",
-			bonusValue: 4,
-			state: "intact",
-			isUsable: true,
-		},
-
-		// --- Pickaxes ---
-		{
-			id: "item_iron_pickaxe_01",
-			visual: "item_iron_pickaxe",
-			name: "Iron Pickaxe",
-			category: "pickaxe",
-			bonusValue: 2,
-			state: "intact",
-			isUsable: true,
-		},
-		{
-			id: "item_iron_pickaxe_02",
-			visual: "item_iron_pickaxe",
-			name: "Iron Pickaxe",
-			category: "pickaxe",
-			bonusValue: 2,
-			state: "damaged",
-			isUsable: true,
-		}
-	];
-	return testInv;
-}
-
-
 export function exploreLandscape(gameState, renderCallback, landscapeCard) {
 
-	const testInv = buildTestInv();
+	const playerInventory = gameState.players[gameState.currentPlayerIndex].inventory
 	const debug = document.getElementById("modal-debug");
+
 	const actionModal = document.getElementById("action-modal");
 	actionModal.classList.remove("hidden");
 	const modalCardInfo = document.getElementById("modal-card-info");
@@ -46,15 +12,17 @@ export function exploreLandscape(gameState, renderCallback, landscapeCard) {
 	landscapeCardView.classList.add("card", "modal-landscape");
 	modalCardInfo.appendChild(landscapeCardView);
 	const actionButtons = document.getElementById("modal-action-buttons");
-	actionButtons.innerHTML='';
+	actionButtons.innerHTML = '';
+	let cost = landscapeCard.cost;
+	const playerTools = playerInventory.filter(item => item.category === landscapeCard.toolDiscount);
+	const toolDiscounts = {};
 
 	//------------
 	//Buttons
 	//------------
 	if (landscapeCard.subtype === "explore") {
-		let cost = landscapeCard.cost;
 		//Explore Button
-		
+
 		const exploreButton = document.createElement("button")
 		exploreButton.id = "explore-button";
 		exploreButton.classList.add("tap-control", "button");
@@ -65,12 +33,8 @@ export function exploreLandscape(gameState, renderCallback, landscapeCard) {
 		actionButtons.appendChild(exploreButton);
 
 		if (landscapeCard.toolDiscount) {
-			const playerInventory = gameState.players[gameState.currentPlayerIndex].inventory;
-			//Change to actual inventory!!
-			const playerTools = testInv.filter(item => item.category === landscapeCard.toolDiscount);
-
 			if (playerTools.length > 0) {
-				const toolDiscounts = {};
+
 				playerTools.forEach((tool) => {
 					const useChoiceGroup = document.createElement("div")
 					const uses = (tool.state === "intact") ? "(2/2)" : "(1/2)";
@@ -90,7 +54,6 @@ export function exploreLandscape(gameState, renderCallback, landscapeCard) {
 						}
 					}
 					useSelect.addEventListener("change", (event) => {
-						debug.innerHTML = `cost=${cost} disc=${event.target.value}`
 						toolDiscounts[tool.id] = parseInt(event.target.value);
 						const totalDiscount = Object.values(toolDiscounts).reduce((sum, val) => sum + val, 0);
 						cost = Math.max(landscapeCard.cost - totalDiscount, 0);
@@ -102,20 +65,37 @@ export function exploreLandscape(gameState, renderCallback, landscapeCard) {
 					});
 					useChoiceGroup.appendChild(useSelect);
 					actionButtons.appendChild(useChoiceGroup);
-
-
 				});
 			}
 		}
 	}
-const backButton = document.createElement("button");
-backButton.classList.add("tap-control","button");
-backButton.textContent = "Go Back";
-backButton.addEventListener("click", () => {
-    actionModal.classList.add("hidden");
-});
-actionButtons.appendChild(backButton);	
-								
-	
-function executeExplore() {}
+	const backButton = document.createElement("button");
+	backButton.classList.add("tap-control", "button");
+	backButton.textContent = "Go Back";
+	backButton.addEventListener("click", () => {
+		actionModal.classList.add("hidden");
+	});
+	actionButtons.appendChild(backButton);
+
+
+	function executeExplore() {
+		gameState.hungerRemaining -= cost;
+		let index = 0;
+		playerTools.forEach((tool) => {
+			if (toolDiscounts[tool.id] > 0) {
+				const uses = toolDiscounts[tool.id] / tool.bonusValue;
+				if (tool.state === "damaged") {
+					tool.state = "broken";
+					index = playerInventory.indexOf(tool);
+					playerInventory.splice(index, 1);
+				} else if (uses === 1) {
+					tool.state = "damaged"
+				} else {
+					tool.state = "broken";
+					index = playerInventory.indexOf(tool);
+					playerInventory.splice(index, 1);
+				}
+			}
+		});
+	}
 }
